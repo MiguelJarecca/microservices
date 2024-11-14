7dd98843ac79390d358fa85d8113ded05758afc8
miguel <miguel.jarecca@gmail.com>
2024-11-14 11:36:17 -0500

    se puede ingresar un cliente y se crea su cuenta automaticamente

---
+++ sesion2/accounts/src/main/java/com/easybytes/accounts/constans/AccountsConstants.java
@@ -0,0 +1,18 @@
+package com.easybytes.accounts.constans;
+
+public class AccountsConstants {
+
+    private AccountsConstants() {
+        // restrict instantiation
+    }
+
+    public static final String SAVINGS = "savings";
+    public static final String ADDRESS = "123 Main Street, New York";
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGES_201 = "Account created Successful";
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGES_200 = "Request processed Successful";
+    public static final String STATUS_500 = "500";
+    public static final String MESSAGES_500 = "An error occured. PLease try again or contact Dev tem";
+
+}
