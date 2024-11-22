ba1f8de6a5ea18d57b9207817c124f63abec776a
miguel <miguel.jarecca@gmail.com>
2024-11-22 12:55:47 -0500

    se termina la documentacion de api rest accounts

--- sesion2/accounts/src/main/java/com/easybytes/accounts/constans/AccountsConstants.java
+++ sesion2/accounts/src/main/java/com/easybytes/accounts/constans/AccountsConstants.java
@@ -6,13 +6,16 @@ public class AccountsConstants {
         // restrict instantiation
     }
 
-    public static final String SAVINGS = "savings";
-    public static final String ADDRESS = "123 Main Street, New York";
-    public static final String STATUS_201 = "201";
-    public static final String MESSAGES_201 = "Account created Successful";
-    public static final String STATUS_200 = "200";
-    public static final String MESSAGES_200 = "Request processed Successful";
-    public static final String STATUS_500 = "500";
-    public static final String MESSAGES_500 = "An error occured. PLease try again or contact Dev tem";
+    public static final String  SAVINGS = "Savings";
+    public static final String  ADDRESS = "123 Main Street, New York";
+    public static final String  STATUS_201 = "201";
+    public static final String  MESSAGE_201 = "Account created successfully";
+    public static final String  STATUS_200 = "200";
+    public static final String  MESSAGE_200 = "Request processed successfully";
+    public static final String  STATUS_417 = "417";
+    public static final String  MESSAGE_417_UPDATE= "Update operation failed. Please try again or contact Dev team";
+    public static final String  MESSAGE_417_DELETE= "Delete operation failed. Please try again or contact Dev team";
+    // public static final String  STATUS_500 = "500";
+    // public static final String  MESSAGE_500 = "An error occurred. Please try again or contact Dev team";
 
 }
