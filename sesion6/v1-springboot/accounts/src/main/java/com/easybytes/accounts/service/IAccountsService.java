84344114fdd941966e326d2f2f666ccdb90d0499
miguel <miguel.jarecca@gmail.com>
2025-01-28 16:17:08 -0500

    se trae la seccion 4 para continuar

---
+++ sesion6/v1-springboot/accounts/src/main/java/com/easybytes/accounts/service/IAccountsService.java
@@ -0,0 +1,34 @@
+package com.easybytes.accounts.service;
+
+import com.easybytes.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+
+    /**
+     *
+     * @param customerDto - customerDto Object
+     */
+    void createAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber - input Mobile Number
+     * @return Accounts details based on a given mobileNumber
+     */
+    CustomerDto fetchAccount(String mobileNumber);
+
+    /**
+     *
+     * @param customerDto Object
+     * @return boolean indicating if the update of account details if successful or not
+     */
+    boolean updateAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return boolean indicating if the delete of Account details is successful or not
+     */
+    boolean deleteAccount(String mobileNumber);
+
+}
