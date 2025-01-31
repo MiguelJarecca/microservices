ef5effaf3e692b76c46f834a5a7fbe9a4c94da94
miguel <miguel.jarecca@gmail.com>
2025-01-30 19:26:04 -0500

    se configura diferentes perfiles a la app

--- sesion6/v1-springboot/accounts/src/main/java/com/easybytes/accounts/dto/AccountsContactInfoDto.java
+++ sesion6/v1-springboot/accounts/src/main/java/com/easybytes/accounts/dto/AccountsContactInfoDto.java
@@ -8,5 +8,4 @@ import java.util.Map;
 @ConfigurationProperties(prefix = "accounts")
 public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
 
-
 }
